cb9d546aff2ba20f2edf8a61640ef87ba5cf9ab7
yanguly <yanguly@gmail.com>
2015-12-23 18:49:04 +0200

    Initial Commit

---
+++ SecondLabSecondPart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecondLabSecondPart
+//
+//  Created by Yan on 12/23/15.
+//  Copyright © 2015 DianaVolodchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SecondLabSecondPart/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SecondLabSecondPart
+//
+//  Created by Yan on 12/23/15.
+//  Copyright © 2015 DianaVolodchenko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
